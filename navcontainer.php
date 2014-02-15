8c79b5c83ed341eafb5c8d952ff69d4f2ee5c4e7
redduckmusic <redduckmusic@earthlink.net>
2014-02-15 14:57:07 -0800

    Update navcontainer.php

--- navcontainer.php
+++ navcontainer.php
@@ -35,5 +35,5 @@ function genActive($item)
 			<li><a href="http://www.ccs.ucsb.edu/primavera/2007/">Primavera 2007</a></li>
 			<li class="break"><a href="http://www.ccs.ucsb.edu/primavera/2006/">Primavera 2006</a></li>
 		</ul>
-<span><img src="images/incendia_ex_loxodrome_by_nic022-d66y3ho.jpg" width="250"  alt="Floral Loxodrome, by Nicholas Jackson"/></span>
+<span><img src="images/incendia_ex_loxodrome_by_nic022-d66y3ho_SCALED.jpg" width="250"  alt="Floral Loxodrome, by Nicholas Jackson"/></span>
 	</div>
