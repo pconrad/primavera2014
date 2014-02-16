9a5d57d6c02f32547dc3477d9f9586bc5f2f748f
redduckmusic <redduckmusic@earthlink.net>
2014-02-16 09:18:50 -0800

    Update press.php
    added press from 2009 onward

--- press.php
+++ press.php
@@ -20,6 +20,12 @@
 	
 	<h2>Press</h2>
 	
+	<p>&ldquo;Each year, the vernal festival phenomenon known as the "Primavera Festival" spreads its wings at UCSB, with an emphasis on showcasing the more new and experimental energies and resources on campus. Generally, we hear music by faculty, students and contemporary composers &mdash; in both the real time analogue world and the digital realm &mdash; via the Ensemble for Contemporary Music and the computer music&#45;intensive acronym CREATE. This year's festival, a string of concerts, events and exhibitions last week, had as a visiting dignitary element during Wednesday's CREATE concert an appearance by famed contemporary baritone Nicholas Isherwood. He performed music of Karlheinz Stockhausen, written for the baritone shortly before the toweringly influential German composer passed away in 2007.&rdquo;</p>
+	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2013</p>
+	
+	<p>&ldquo;Casual passers&#45;by Tuesday night, near the UCSB Music Department courtyard, might have been alarmed or at least intrigued by the unconventional sights and sounds there. An abstract, 45&#45;minute computer music piece was being pumped through loudspeakers, played on laptops held by slowly ambling, zombie&#45;like folks in black masks. Some might have guessed creative foul play of the artistic sort, the result of student&#45;spawned guerilla experimentalism&mdash;not uncommon in an academic/artistic setting. But those in the know might have made another timely assumption: it's Primavera time.&rdquo;</p>
+	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2009</p>
+
 	<p>&ldquo;If the stated ambition of UCSB&rsquo;s Primavera festival, organized by Jeremy Haladyna, is to find junctures between the worlds of different musical ideas, media and machinery, the Friday night of the festival lived up to the mission. By contrast with conventional musical settings encountered in classical music, the music leaned into the margins, where fresh ideas and contexts are formed and experimented with.&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2007</p>
 
