f70014ea2bfecff90e7be241dfa3fc2126e4b7d1
redduckmusic <redduckmusic@earthlink.net>
2014-02-16 09:22:36 -0800

    Update press.php

--- press.php
+++ press.php
@@ -25,16 +25,16 @@
 	<br />
 	<p>&ldquo;Casual passers&#45;by Tuesday night, near the UCSB Music Department courtyard, might have been alarmed or at least intrigued by the unconventional sights and sounds there. An abstract, 45&#45;minute computer music piece was being pumped through loudspeakers, played on laptops held by slowly ambling, zombie&#45;like folks in black masks. Some might have guessed creative foul play of the artistic sort, the result of student&#45;spawned guerilla experimentalism&mdash;not uncommon in an academic/artistic setting. But those in the know might have made another timely assumption: it's Primavera time.&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2009</p>
-
+	<br />
 	<p>&ldquo;If the stated ambition of UCSB&rsquo;s Primavera festival, organized by Jeremy Haladyna, is to find junctures between the worlds of different musical ideas, media and machinery, the Friday night of the festival lived up to the mission. By contrast with conventional musical settings encountered in classical music, the music leaned into the margins, where fresh ideas and contexts are formed and experimented with.&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2007</p>
-
+	<br />
 	<p>&ldquo;An impression conveyed through this key Primavera concert was this: clearly a new energy is abuzz at the university, with the arrival of Barlow and the growth of interdisciplinary and technological departments. And, one of its pet names is &lsquo;multimedia.&rsquo;&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2007</p>
-	
+	<br />	
 	<p>&ldquo;For over a decade, the UCSB New Music Festival has been a key cultural event in Santa Barbara, often bringing in guest composers and provocative themes from the music world for spring flings of varying intensity. With the retirement of founding composer William Kraft last year, the festival is in new hands, and with a new concept at its center. UCSB&rsquo;s Jeremy Haladyna, the associate director of both the New Music Festival and the Ensemble for Contemporary Music, has rethought the festival, expanded it to include such university-based resources as the dance and the technology-based MAT program, and has accordingly given the festival a new name, &lsquo;Primavera.&rsquo;&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2003</p>
-	
+	<br />	
 	<p>&ldquo;It&rsquo;s fitting that the Primavera Festival, progeny of the old UCSB New Music Festival, is hosting hard-to-define out-of-towners. Last Monday, it was Laetitia Sonami blurring the boundaries between computer music, performance art, sound art, et cetera. As the festival heads into its main weekend events, the star of the show is the noted new music baritone Nicholas Isherwood, who has performed extensively not only with Karlheinz Stockhausen, Maurizio Kagel, and other prominent contemporary composers, including John Cage, but who has also worked with inventive jazzer Steve Lacy.&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Independent</cite>, 2003</p>
 	
