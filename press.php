adc08e8629865fa9042107415c1e2b0735ddfe78
redduckmusic <redduckmusic@earthlink.net>
2014-02-16 09:20:49 -0800

    Update press.php

--- press.php
+++ press.php
@@ -22,7 +22,7 @@
 	
 	<p>&ldquo;Each year, the vernal festival phenomenon known as the "Primavera Festival" spreads its wings at UCSB, with an emphasis on showcasing the more new and experimental energies and resources on campus. Generally, we hear music by faculty, students and contemporary composers &mdash; in both the real time analogue world and the digital realm &mdash; via the Ensemble for Contemporary Music and the computer music&#45;intensive acronym CREATE. This year's festival, a string of concerts, events and exhibitions last week, had as a visiting dignitary element during Wednesday's CREATE concert an appearance by famed contemporary baritone Nicholas Isherwood. He performed music of Karlheinz Stockhausen, written for the baritone shortly before the toweringly influential German composer passed away in 2007.&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2013</p>
-	
+	<br />
 	<p>&ldquo;Casual passers&#45;by Tuesday night, near the UCSB Music Department courtyard, might have been alarmed or at least intrigued by the unconventional sights and sounds there. An abstract, 45&#45;minute computer music piece was being pumped through loudspeakers, played on laptops held by slowly ambling, zombie&#45;like folks in black masks. Some might have guessed creative foul play of the artistic sort, the result of student&#45;spawned guerilla experimentalism&mdash;not uncommon in an academic/artistic setting. But those in the know might have made another timely assumption: it's Primavera time.&rdquo;</p>
 	<p>&mdash; Joe Woodard, <cite>Santa Barbara News Press</cite>, 2009</p>
 
